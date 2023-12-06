ac86294203703a6275632f1b8d63b3a559e30b28
IvanVance <vance051@gmail.com>
2023-12-06 22:25:43 +0300

    HW1 - описан тип MessagePropsType, отображены приходящие данные

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -12,19 +12,19 @@ import avatar from './avatar.png'
 * 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
 * 5 - сделать стили в соответствии с дизайном
 * */
-type Usertype = {
-    avatar: string
-    name: string
-}
-type TextType = {
-    text: string
-    time: string
-}
+
 // нужно создать правильный тип вместо any
 export type MessageType = {
-    id:number
-    user: Usertype
-    message: TextType
+    id: number
+    user: {
+        avatar: string
+        name: string
+    }
+
+    message: {
+        text: string
+        time: string
+    }
 }
 
 // структуру объекта не менять
